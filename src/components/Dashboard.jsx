ec0fc8e9685b2d46f0bd93054048e5411539bf0b
dolisha <dolishagandhi@gmail.com>
2026-05-07 00:19:56 +0530

    api

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = () => {
     setLoading(true);
 
     const res = await fetch(
-      `https://api.openalex.org/institutions?filter=country_code:${countryCodes[country]}&per-page=10`
+      `https://api.openalex.org/institutions?filter=country_code:${countryCodes[country]}&per-page=100`
     );
     const data = await res.json();
     setColleges(data.results || []);
